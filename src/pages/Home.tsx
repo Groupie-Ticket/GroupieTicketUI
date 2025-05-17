0f53b8dc9be7bfc6d3db77a096db5327ab069291
Facundo Leonel Isoler Pagani <facundoisoler@outlook.com>
2025-05-17 08:48:44 -0300

    feat: update Home to use HomeContentNew

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import Hero from '../sections/Hero';
-import HomeContent from '../sections/HomeContent';
+// import HomeContent from '../sections/HomeContent';
+import HomeContentNew from '../sections/HomeContentNew';
 // import Reproductor from '../sections/Reproductor';
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <div className="w-full min-h-screen flex flex-col bg-white">
       <Hero />
       {/* <Reproductor /> */}
-      <HomeContent />
+      {/* <HomeContent /> */}
+      <HomeContentNew  />
     </div>
   );
 }
